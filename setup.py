f2139a524a7a6121dbad3994a73bd9b27565158b
Jim Brännlund <jbrannlund@planview.com>
2016-12-23 16:17:15 +0100

    Adding pypi setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='basepage',
+    packages=['basepage'],
+    version='0.1',
+    description='A wrapper for WebDriver that provides some extended and additional functionality',
+    author='Jim Brannlund',
+    author_email='jim.brannlund@gmail.com',
+    url='https://github.com/BeyondEvil/basepage',
+    download_url='https://github.com/peterldowns/mypackage/tarball/0.1',
+    keywords=['testing', 'webdriver', 'selenium'],
+    classifiers=[],
+)
