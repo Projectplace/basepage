735476877eb100db0981590a6d12140e68652167
Biwwe <baziz@planview.com>
2019-01-03 10:37:07 +0100

    Updating version number

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='basepage',
     packages=['basepage'],
-    version='1.7.1',
+    version='1.7.2',
     description='A wrapper for WebDriver that provides some extended and additional functionality',
     author='Bijwen Aziz',
     author_email='baziz@planview.com',
     url='https://github.com/Projectplace/basepage',
-    download_url='https://github.com/Projectplace/basepage/tarball/1.7.1',
+    download_url='https://github.com/Projectplace/basepage/tarball/1.7.2',
     keywords=['testing', 'webdriver', 'selenium'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
